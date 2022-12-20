433fa4a79e857a0f779444c6993a20b9d9279342
alpay <calalli1903@gmail.com>
2022-12-20 16:40:30 +0400

    Initial Commit

---
+++ ClothesApp/ClothesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesAppApp.swift
+//  ClothesApp
+//
+//  Created by Alpay Calalli on 20.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ClothesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothesApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClothesApp
+//
+//  Created by Alpay Calalli on 20.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
